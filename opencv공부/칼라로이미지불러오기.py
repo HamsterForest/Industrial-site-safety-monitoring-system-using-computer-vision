93ebe59b39b92d48071ad3f45fb3212c063cb2be
HamsterForest <republic7788@gmail.com>
2023-03-09 19:54:55 +0900

    practice

---
+++ opencv공부/칼라로이미지불러오기.py
@@ -0,0 +1,7 @@
+import cv2
+
+img_color=cv2.imread('3.jpg',cv2.IMREAD_COLOR)
+cv2.namedWindow("cup3")
+cv2.imshow('cup3',img_color)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ opencv공부/흑백으로변환.py
@@ -0,0 +1,13 @@
+import cv2
+
+img_color=cv2.imread('3.jpg',cv2.IMREAD_COLOR)
+
+img_gray=cv2.cvtColor(img_color,cv2.COLOR_BGR2GRAY)
+
+cv2.namedWindow("cup3")
+
+cv2.imshow('cup3',img_color)
+cv2.waitKey(0)
+cv2.imshow('cup3',img_gray)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
