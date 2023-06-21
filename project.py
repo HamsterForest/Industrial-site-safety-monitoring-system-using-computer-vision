a2a2a2e1cb8ad9a93337601ebae0809cedeeaa0e
HamsterForest <republic7788@gmail.com>
2023-06-21 14:11:59 +0900

    적치물제한구역

--- project.py
+++ project.py
@@ -256,7 +256,11 @@ def draw_caution(frame, idxs, type):
                     count_caut_font_scale, count_caut_color, count_caut_thickness, cv2.LINE_AA)
         return frame
     elif type==3:
-        pass
+        cv2.rectangle(frame, count_caut_cor_back, (count_caut_cor[0]+225, count_caut_cor[1]+5), (0, 0, 0), -1)
+        cv2.circle(frame,(count_caut_cor[0]-45,count_caut_cor[1]-5),15,(0,0,255),-1)
+        cv2.putText(frame, 'Caution : Stockpiles in area', count_caut_cor, font, 
+                    count_caut_font_scale, count_caut_color, count_caut_thickness, cv2.LINE_AA)
+        return frame
     elif type==4:
         cv2.rectangle(frame, count_caut_cor_back, count_caut_cor_back2, (0, 0, 0), -1)
         cv2.circle(frame,(count_caut_cor[0]-45,count_caut_cor[1]-5),15,(0,0,255),-1)
@@ -318,6 +322,194 @@ def auto_range_filter(numbers, mod, term):
         return min(duplicates)
     else:
         return max(duplicates)    
+
+def stockpiled_monitoring_core(fgbg, current_time, objects, objects_duration, frame, pt1, pt2):
+    copied_frame=frame.copy()
+    frame=cut_frame(frame,pt1,pt2)
+    #가우시안 블러링-엣지검출전 노이즈 제거
+    blur=cv2.GaussianBlur(frame,(15,15),5)
+    #전경마스크 얻기
+    fgmask=fgbg.apply(blur,learningRate=0)
+    #모폴로지-타원형 커널
+    kernel=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(51,51))
+    #모폴로지-클로징-흰색영역의 검은색 구멍을 메운다
+    fgmask=cv2.morphologyEx(fgmask,cv2.MORPH_CLOSE,kernel)
+    nlabels, labels, stats, centroids = cv2.connectedComponentsWithStats(fgmask)
+    red_counts=0
+    for index, centroid in enumerate(centroids):
+            if stats[index][0] == 0 and stats[index][1] == 0:
+                continue
+            if np.any(np.isnan(centroid)):
+                continue
+            
+            x, y, width, height, area = stats[index]
+            centerX, centerY = int(centroid[0]), int(centroid[1])
+
+            if area > 100:
+                #오브젝트 안에 인덱스 존재
+                if index in objects:
+                    #abs 절대값 함수, 오자범위 안에 있는지 확인 사이즈 40% 중심점 흔들림 20% 기준
+                    if  abs(width - objects[index][0]) / objects[index][0] <= 0.4 and \
+                        abs(height - objects[index][1]) / objects[index][1] <= 0.4 and \
+                        abs(centerX - objects[index][2]) / objects[index][2] <= 0.2 and \
+                        abs(centerY - objects[index][3]) / objects[index][3] <= 0.2:
+                        # 3초이상 적치시 파란색으로 경계 바꾸어 그림 아니면 빨간색
+                        if current_time - objects_duration[index] >= 3:
+                            red_counts+=1
+                            cv2.rectangle(copied_frame, (x+pt1[0], y+pt1[1]), (x + width + pt1[0], y + height+pt1[1]), (255, 0, 0), 2)
+                        else:
+                            cv2.rectangle(copied_frame, (x+pt1[0], y+pt1[1]), (x + width + pt1[0], y + height+pt1[1]), (0, 0, 255), 2)
+                    else:
+                        # 오브젝트 사이즈와 중심점 좌표, 시간 업데이트(오차범위 안에 없으니 시간 초기화)
+                        objects[index] = [width, height, centerX, centerY]
+                        objects_duration[index] = current_time
+                else:
+                    # 새로운 오브젝트 추가
+                    objects[index] = [width, height, centerX, centerY]
+                    objects_duration[index] = current_time
+
+                cv2.circle(copied_frame, (centerX+pt1[0], centerY+pt1[1]), 1, (0, 255, 0), 2)
+
+    return copied_frame, objects, objects_duration,red_counts
+
+#적치물제한구역모니터링
+def stockpiled_monitoring(term, auto_range):
+    # 비디오 업로드
+    cap = cv2.VideoCapture('videos/object3.mp4')
+    fgbg = cv2.createBackgroundSubtractorMOG2(history=500, varThreshold=250, detectShadows=False)
+    lr = 0
+    objects = {}
+    objects_duration = {}
+    root.withdraw()#인터페이스 숨기기
+    #isDragging => 마우스를 드래그 중인가
+    #x0_m, y0_m, w_m, h_m => 마우스로 그린 사각형 좌표
+    #prev_time => yolo term 관련
+    #initial_flag => initial_flag가 1일 때만 욜로를 진행한다. yolo term과 관련됨.
+    #usef_flag => 1이면 사용자지정범위가 켜진다.
+    global isDragging, x0_m, y0_m, w_m, h_m, prev_time, initial_flag, user_flag
+    auto_range_on=0#auto_range하고 있는 중인가?
+    auto_range_on_count=0#auto_range하고 얼마나 욜로에 진입했나?
+    red_counts=0
+
+    while True:
+        #사용자 지정 범위의 변수선언
+        pt1=(x0_m,y0_m)
+        pt2=(x0_m+w_m,y0_m+h_m)
+        
+        #비디오캡쳐
+        ret, frame = cap.read()
+        #비디오 없으면 종료
+        if not ret:
+            break
+        #비디오 사이즈 재조정
+        frame = cv2.resize(frame, (640, 480))
+        
+        current_time = time.time()
+
+        #자동범위조정 설정시 사용자 지정범위 끄기=> 나중에 다시 켜야함.
+        if auto_range==1:
+            auto_range_on=1
+            user_flag=-1#사용자지정범위 끄기
+
+        #사용자 지정 범위 
+        cv2.imshow('frame', frame)
+        cv2.setMouseCallback('frame', onMouse)
+        #모니터링 바운더리 사각형 그려지는 곳-욜로수행범위를 실질적으로 제한하는 것은 pt1 pt2의 변경
+        if w_m>0 and h_m>0 and user_flag==1:
+            if w_m<100 or h_m<100: # 작은 사각형은 다른 색 사용
+                cv2.rectangle(frame, pt1, pt2, (0, 0, 255), 2)
+            else:
+                cv2.rectangle(frame, pt1, pt2, (255, 0, 0), 3)
+            if isDragging==False and (w_m<100 or h_m<100): # 최종적으로 너무 작은 상자가 그려지면 지워짐
+                w_m=0
+                h_m=0
+        
+        #자동범위 확정적 그리기
+        if auto_range_on!=1 and user_flag!=1:#자동범위 실행중에는 그리지 않음
+            cv2.rectangle(frame, pt1, pt2, (255, 0, 0), 3)
+
+        #yolo term 조절, yolo 진입, auto_range도 여기서 진입
+        if initial_flag==True:
+            prev_time = time.time()
+            
+            #자동 범위 조정 메인
+            if auto_range==1 and auto_range_on==1:
+                idxs, boxes=auto_boundary(frame)
+                auto_range_on_count+=1
+                if auto_range_on_count==(term*10):#10초동안 진행
+                    auto_range_on=0#auto range종료
+                    auto_range=0
+                    #pt1과 pt2를 지정하면, 그곳에서만 욜로 수행, 그를 위해서 x0_m,y0_m,w_m,h_m 조정
+                    if len(auto_boundary_tops)>=6:
+                        x0_m=auto_range_filter(auto_boundary_lefts,0,term)
+                        y0_m=auto_range_filter(auto_boundary_tops,0,term)
+                        w_m=auto_range_filter(auto_boundary_rights,1,term)-auto_range_filter(auto_boundary_lefts,0,term)
+                        h_m=auto_range_filter(auto_boundary_bottoms,1,term)-auto_range_filter(auto_boundary_tops,0,term)
+                        pt1=(x0_m,y0_m)
+                        pt2=(x0_m+w_m,y0_m+h_m)
+                    elif 0<len(auto_boundary_tops)<6:
+                        x0_m=min(auto_boundary_lefts)
+                        w_m=max(auto_boundary_rights)-min(auto_boundary_lefts)
+                        y0_m=0
+                        h_m=480
+                        pt1=(x0_m,y0_m)
+                        pt2=(x0_m+w_m,y0_m+h_m)
+                    else:
+                        pass # 자동범위 조정 실패 문구
+            initial_flag=False
+
+        #욜로 사람수 세기 메인
+        if (isDragging==False or( isDragging==True and (w_m<0 and h_m<0))) and auto_range!=1:# 드래그 하는 동안에 그리고 사각형이 그려지는 동안에는 욜로하지 않음
+            if user_flag == 1 and w_m>100 and h_m>100:#사용자 범위 설정시 욜로 범위가 너무 작으면 안된다.
+                frame, objects, objects_duration, red_counts=stockpiled_monitoring_core(fgbg,current_time,objects,objects_duration,frame,pt1,pt2)
+            elif len(auto_boundary_tops)>0:#보완이 필요한 부분
+                frame, objects, objects_duration, red_counts=stockpiled_monitoring_core(fgbg,current_time,objects,objects_duration,frame,pt1,pt2)
+            else:
+                frame, objects, objects_duration, red_counts=stockpiled_monitoring_core(fgbg,current_time,objects,objects_duration,frame,(0,0),(640,480))
+
+        #사람수 표시는 항상 그린다.
+        #사람수 text배경 -검은색
+        if auto_range!=1:
+            cv2.rectangle(frame, offlimit_cor_back, offlimit_cor_back2, (0, 0, 0), -1)
+            cv2.putText(frame, 'storage restrictions monitoring', offlimit_cor, font, 
+                        offlimit_font_scale, offlimit_color, offlimit_thickness, cv2.LINE_AA)
+        else:#자동 범위 설정중일 경우
+            cv2.rectangle(frame, count_cor_back, count_cor_back2, (0, 0, 0), -1)
+            cv2.putText(frame, 'Detecting Traffic cone', count_cor, font, 
+                        count_font_scale, count_color, count_thickness, cv2.LINE_AA)
+        
+        #사람수 확인 및 경고 표현
+        if red_counts>0 and auto_range!=1:
+            frame=draw_caution(frame, 0,3)
+
+        #YOLO텀 조절용2
+        lapsed_time = time.time() - prev_time
+        if lapsed_time > (1./ term):
+            initial_flag=True
+
+        #자동범위에서 라바콘 그리기
+        if auto_range==1:
+            if len(idxs)>0:
+                for i in idxs.flatten():
+                    box=boxes[i]
+                    left=box[0]
+                    top=box[1]
+                    w=box[2]
+                    h=box[3]
+                    cv2.rectangle(frame, (left, top), (left+w, top+h), (0, 255, 0), 2)
+        
+        cv2.imshow('frame', frame)
+
+        keycode=cv2.waitKey(25)
+        if keycode == 27:#esc키 종료
+            break
+        elif keycode == ord('u'):# 사용자 지정 범위 끄기
+            user_flag=-1
+        elif keycode == ord('r'):# 디폴트화면 리셋
+            fgbg=cv2.createBackgroundSubtractorMOG2(history=500,varThreshold=250,detectShadows=False)
+    cap.release()
+    cv2.destroyAllWindows()
+    root.deiconify()#인터페이스 다시 등장
 
 #접근금지구역모니터링
 def off_limit_monitoring(term,auto_range):
@@ -381,14 +573,14 @@ def off_limit_monitoring(term,auto_range):
                     auto_range_on=0#auto range종료
                     auto_range=0
                     #pt1과 pt2를 지정하면, 그곳에서만 욜로 수행, 그를 위해서 x0_m,y0_m,w_m,h_m 조정
-                    if len(auto_boundary_tops)>0:
+                    if len(auto_boundary_tops)>=6:
                         x0_m=auto_range_filter(auto_boundary_lefts,0,term)
                         y0_m=auto_range_filter(auto_boundary_tops,0,term)
                         w_m=auto_range_filter(auto_boundary_rights,1,term)-auto_range_filter(auto_boundary_lefts,0,term)
                         h_m=auto_range_filter(auto_boundary_bottoms,1,term)-auto_range_filter(auto_boundary_tops,0,term)
                         pt1=(x0_m,y0_m)
                         pt2=(x0_m+w_m,y0_m+h_m)
-                    elif len(auto_boundary_tops)<6:
+                    elif 0<len(auto_boundary_tops)<6:
                         x0_m=min(auto_boundary_lefts)
                         w_m=max(auto_boundary_rights)-min(auto_boundary_lefts)
                         y0_m=0
@@ -535,7 +727,7 @@ def workers_counts_monitoring(num, term, auto_range):#num은 할당 인원수
                         h_m=auto_range_filter(auto_boundary_bottoms,1,term)-auto_range_filter(auto_boundary_tops,0,term)
                         pt1=(x0_m,y0_m)
                         pt2=(x0_m+w_m,y0_m+h_m)
-                    elif 1<len(auto_boundary_tops)<6:
+                    elif 0<len(auto_boundary_tops)<6:
                         x0_m=min(auto_boundary_lefts)
                         w_m=max(auto_boundary_rights)-min(auto_boundary_lefts)
                         y0_m=0
@@ -683,7 +875,7 @@ def helmet_monitoring(term, auto_range):#num은 할당 인원수
                         h_m=auto_range_filter(auto_boundary_bottoms,1,term)-auto_range_filter(auto_boundary_tops,0,term)
                         pt1=(x0_m,y0_m)
                         pt2=(x0_m+w_m,y0_m+h_m)
-                    elif 1<len(auto_boundary_tops)<6:
+                    elif 0<len(auto_boundary_tops)<6:
                         x0_m=min(auto_boundary_lefts)
                         w_m=max(auto_boundary_rights)-min(auto_boundary_lefts)
                         y0_m=0
@@ -772,6 +964,8 @@ def main_loop(btn,allocated,term,auto_range):
         off_limit_monitoring(term,auto_range)
     elif btn==2:
         workers_counts_monitoring(allocated,term,auto_range)#할당인원, term 변수, auto_range여부
+    elif btn==3:
+        stockpiled_monitoring(term, auto_range)
     elif btn==4:
         helmet_monitoring(term,auto_range)
     
@@ -781,7 +975,7 @@ def bck_btn():
         widget.destroy()
     button1 = tk.Button(root, text="접근금지구역 모니터링",command=first_btn)
     button2 = tk.Button(root, text="작업인원수 모니터링",command=second_btn)
-    button3 = tk.Button(root, text="적치물 제한 구역 모니터링")
+    button3 = tk.Button(root, text="적치물 제한 구역 모니터링",command=third_btn)
     button4 = tk.Button(root, text="안전모 착용 모니터링",command=forth_btn)
 
     button1.place(x=50, y=30, width=250, height=50)
@@ -888,6 +1082,54 @@ def second_btn2(allocated, term):
     button6 = tk.Button(root, text="모니터링 시작",command= lambda : main_loop(2,allocated,term,toggle_var.get()))
     button6.place(x=450, y=300, width=150, height=50)
 
+def third_btn():
+    for widget in root.winfo_children():
+        widget.destroy()
+    button4 = tk.Button(root, text="뒤로 가기", command=bck_btn)
+    button4.place(x=30, y=30, width=60, height=30)
+
+    tk.Label(root,text='[적치물제한구역 모니터링]').place(x=50, y=80)
+    tk.Label(root,text='1. 현재 촬영구역의 상태가 비어 있는 상태에서 시작해야 합니다.').place(x=50, y=110)
+    tk.Label(root,text='2. 혹은 비어 있는 상태에서 [r]키를 눌러 빈 방의 상태임을 확정해주세요.').place(x=50, y=140)
+    tk.Label(root,text='[모니터링 빈도 설정]').place(x=50, y=170)
+    tk.Label(root,text='3. 사용자 기기의 성능에 따라 모니터링 빈도를 조절하세요.').place(x=50, y=200)
+    tk.Label(root,text='4. 예를들어 10을 설정하면, 1초에 10번 모니터링 하게 됩니다.').place(x=50, y=230)
+    tk.Label(root,text='5. 8보다 작은 경우, 사람의 경계를 표시하는 바운더리 박스가 그려지지 않습니다.').place(x=50, y=260)
+
+    tk.Label(root,text='모니터링 빈도 : ').place(x=50, y=300)
+    scale1 = tk.Scale(root, from_=1, to=60,orient="horizontal", length=300)
+    scale1.set(10)
+    scale1.place(x=160, y=280,)
+    
+    button5 = tk.Button(root, text="다 음",command= lambda : third_btn2(scale1.get()))
+    button5.place(x=450, y=180, width=150, height=50)
+
+def third_btn2(term):
+    for widget in root.winfo_children():
+        widget.destroy()
+    button4 = tk.Button(root, text="뒤로 가기", command=bck_btn)
+    button4.place(x=30, y=30, width=60, height=30)
+
+    tk.Label(root,text='[범위 자동 할당 설정]').place(x=50, y=80)
+    tk.Label(root,text='1. 자동 할당 기능을 켜는 경우 라바콘을 자동으로 인식하여 범위를 자동으로 할당합니다.').place(x=50, y=110)
+    tk.Label(root,text='2. 자동할당에 실패할 경우 수동범위조작모드로 변경됩니다.').place(x=50, y=140)
+    tk.Label(root,text='3. 수동범위조작모드에서 마우스로 드래그하여 범위를 설정합니다.').place(x=50, y=170)
+    tk.Label(root,text='4. 마우스로 드래그 할 때 파란색이 되어야 범위가 설정됩니다. 너무 작으면 안 됩니다.').place(x=50, y=200)
+    tk.Label(root,text='5. 범위는 u 를 눌러서 초기화 할 수 있습니다.').place(x=50, y=230)
+    tk.Label(root,text='6. r을 누르면 다시 자동으로 범위를 할당 합니다.').place(x=50, y=260)
+    tk.Label(root,text='7. 할당된 이후 드래그 하여 다시 범위를 설정 할 수 있습니다.').place(x=50, y=290)
+    tk.Label(root,text='범위 자동 할당[ON/OFF]').place(x=50, y=320)
+
+    tk.Label(root, text="ON").place(x=200, y=310)
+    toggle_var = tk.IntVar()
+    toggle_check = tk.Checkbutton(root,variable=toggle_var)
+    toggle_check.place(x=200, y=325)
+
+    tk.Label(root,text='종료시에는 ESC키를 눌러서 종료해야 합니다.').place(x=50, y=360)
+
+    button6 = tk.Button(root, text="모니터링 시작",command= lambda : main_loop(3,0,term,toggle_var.get()))
+    button6.place(x=450, y=300, width=150, height=50)
+
 def forth_btn():
     for widget in root.winfo_children():
         widget.destroy()
@@ -943,7 +1185,7 @@ root.resizable(False, False)
 
 button1 = tk.Button(root, text="접근금지구역 모니터링",command=first_btn)
 button2 = tk.Button(root, text="작업인원수 모니터링",command=second_btn)
-button3 = tk.Button(root, text="적치물 제한 구역 모니터링")
+button3 = tk.Button(root, text="적치물 제한 구역 모니터링",command=third_btn)
 button4 = tk.Button(root, text="안전모 착용 모니터링",command=forth_btn)
 
 button1.place(x=50, y=30, width=250, height=50)
